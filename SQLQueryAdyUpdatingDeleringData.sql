c456ae93ac7e1be97014f7d7de446eb936bdd6b0
Adelina Stefanova <156297040+AdelinaStefanova@users.noreply.github.com>
2024-03-17 14:44:26 +0200

    Updating Deleting Data

---
+++ SQLQueryAdyUpdatingDeleringData.sql
@@ -0,0 +1,24 @@
+SELECT *
+FROM EmployeeDemographics;
+
+--we have 1011, NULL, 1013:
+--UPDATE EmployeeDemographics
+--SET EmployeeID = 1012
+--WHERE FirstName = 'Holly' AND LastName = 'Flax'
+
+--Age and Gender are NULL and Male:
+--UPDATE EmployeeDemographics
+--SET Age = 31 , Gender = 'Female'
+--WHERE FirstName = 'Holly' AND LastName = 'Flax'
+
+--DELETE FROM EmployeeDemographics
+--WHERE EmployeeID = 1005
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE EmployeeID = 1004;
+---- and then delete:
+--DELETE
+--FROM EmployeeDemographics
+--WHERE EmployeeID = 1004;
+
